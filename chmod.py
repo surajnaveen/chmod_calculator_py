ad7acb1c9f979413af7d4178e4a0312f49a01f90
Suraj Naveen <suraj0nb@gmail.com>
2025-08-17 19:37:57 +0530

    error_handdle

--- chmod.py
+++ chmod.py
@@ -6,18 +6,20 @@ def ChmodCalculator(text):
         splited_text = string_text.split(',')
         #print(len(splited_text))
 
-        if len(splited_text)<=1:
+        if not len(splited_text)==3:
             print("\n<__please Enter valid input__>")
-    
+            return
+
     except:
-        print("\n<__please Enter valid input__>")
+        print("\n<<__please Enter valid input__>>\n\n")
 
 
 while(Input_text.lower()!='exit'):
-    print("\n__Welcome to chmod_calculator__\n\n--> This simple command line interface program help you to calculater linux chmod values easily")
-    print("--> if you want to exit type \'Exit\' \n\n")
+    print("----------------------------------------------------------")
+    print("__Welcome to chmod_calculator__\n--> This simple CLI program help you to calculater linux chmod values easily")
+    print("--> if you want to exit type \'Exit\' \n")
     print("--> Please enter the access fortmat \'rwx\' for \'Owner\',\'Group\',\'Other\'")
-    print("Ex:- rwx,rw,r")
+    print("Ex:- rwx,rw_,r__")
     print("--> make sure you have the write order: Owner, Group, other")
     print("Enter:-",end='')
     Input_text=input()
