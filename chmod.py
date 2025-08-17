8facc1e85d178817618912a84293230fb14711e0
Suraj Naveen <suraj0nb@gmail.com>
2025-08-17 21:44:19 +0530

    Enhance user prompts and input validation in ChmodCalculator final product

--- chmod.py
+++ chmod.py
@@ -1,5 +1,10 @@
 Input_text = ''
 
+print("----------------------------------------------------------")
+print("__Welcome to chmod_calculator__\n--> This simple CLI program help you to calculater linux chmod values easily")
+print("--> Please enter the access fortmat \'rwx\' for \'Owner\',\'Group\',\'Other\'")
+print("----------------------------------------------------------\n")
+
 def ChmodCalculator(text):
     try:
         string_text = str(text)
@@ -7,7 +12,7 @@ def ChmodCalculator(text):
         #print(len(splited_text))
 
         if not len(splited_text)==3:
-            print("\n<__please Enter valid input__>")
+            print("\n<<__please Enter valid input__>>\n")
             return
         
         chmod = [0,0,0]
@@ -22,19 +27,24 @@ def ChmodCalculator(text):
                     chmod[chmod_index]+=1
             chmod_index+= 1
         
+        for item in chmod:
+            if item > 7:
+                print("\n<__Something went wrong, Enter valid access permission__>\n")
+                return
+        
+        print(f"\n<<__chmod value for \'{Input_text}\' is : {''.join( str(x) for x in chmod)} __>>")
+        print(''.join( str(x) for x in chmod)+'\n')
+        return
 
     except:
         print("\n<<__please Enter valid input__>>\n\n")
 
 
 while(Input_text.lower()!='exit'):
-    print("----------------------------------------------------------")
-    print("__Welcome to chmod_calculator__\n--> This simple CLI program help you to calculater linux chmod values easily")
-    print("--> if you want to exit type \'Exit\' \n")
-    print("--> Please enter the access fortmat \'rwx\' for \'Owner\',\'Group\',\'Other\'")
-    print("Ex:- rwx,rw_,r__")
+    print("--> if you want to exit type \'Exit\'")
     print("--> make sure you have the write order: Owner, Group, other")
+    print("Ex:- rwx,rw_,r__")
     print("Enter:-",end='')
     Input_text=input()
 
-    ChmodCalculator(Input_text.lower())
+    chmod_value = ChmodCalculator(Input_text.lower())
